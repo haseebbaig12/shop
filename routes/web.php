83f712b6b0d3a425ca767e623a11edd76f7f2985
haseeb baig <abdulhaseeb1201835@gmail.com>
2021-03-09 18:30:28 +0500

    commit by superspark

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Backend\Dashboard\HomeController;
+use App\Http\Controllers\Backend\Brand\BrandController;
+use App\Http\Controllers\Backend\Slider\SliderController;
+use App\Http\Controllers\Backend\Category\CategoryController;
+use App\Http\Controllers\Backend\Product\ProductController;
+use App\Http\Controllers\Backend\Attribute\AttributeController;
+use App\Http\Controllers\Backend\Local\SiteController;
+use App\Http\Controllers\Backend\Language\LanguageChangeController;
+use App\Http\Controllers\Backend\Language\LanguageController;
+use App\Http\Controllers\Backend\Currency\CurrencyController;
+use App\Http\Controllers\Backend\Local\LocaleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +26,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['site'])->group(function () {
+
+
+//Route::get('/dashboardd', function () {
+//    return view('Dashboard/Dashboard');
+//})->middleware(['auth'])->name('Dashboard');
+    Route::get('/Dashboard',[HomeController::class,'index']);;
+    Route::resource('/brand', BrandController::class);
+    Route::resource('/slider', SliderController::class);
+    Route::resource('/category', CategoryController::class);
+    Route::resource('/product', ProductController::class);
+    Route::resource('/attribute', AttributeController::class);
+    Route::post('/products/{id}', [ProductController::class,'update']);
+    Route::resource('/site', SiteController::class);
+    Route::resource('/lang', LanguageChangeController::class);
+    Route::resource('/language', LanguageController::class);
+    Route::resource('/currency', CurrencyController::class);
+    Route::resource('/locale', LocaleController::class);
+    Route::post('/categori/{id}', [CategoryController::class,'update']);
+    Route::post('demo/{id}', [CategoryController::class,'update']);
+});
+require __DIR__.'/auth.php';
