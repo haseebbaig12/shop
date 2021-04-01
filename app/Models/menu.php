fae345f650941725460c464e2e635f9f406c684d
Abdul Mannan <abdul520mannan@gmail.com>
2021-04-01 16:50:45 +0500

    abdul
    Signed-off-by: Abdul Mannan <abdul520mannan@gmail.com>

--- app/Models/menu.php
+++ app/Models/menu.php
@@ -2,15 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class menu extends Model
+class Menu extends Model
 {
-    protected $table = 'menus';
-    protected $fillable =[
-        'itemID',
-        'itemTitle',
-        'itemSlug'
-    ];
-
+    use HasFactory;
 }
